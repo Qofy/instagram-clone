99ac7b7db861aa38d22bc19fa0086e693d9d8095
Qofy <148817944+Qofy@users.noreply.github.com>
2024-09-01 22:25:05 +0200

    first commit(1)

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import LeftSidebar from './components/LeftSidebar'
+import './App.css'
+
+function App() {
+
+  return (
+    <div className='main-container'>
+     <LeftSidebar/>
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/LeftSidebar.jsx
@@ -0,0 +1,65 @@
+import '../styles/left-sidebar.css';
+import { House, Search, Compass,Tv, Send, Heart, BadgePlus, User } from 'lucide-react';
+
+const nav = [
+  {
+    id: 0,
+    name: 'Home',
+    icon: <House />,
+  },
+  {
+    id: 1,
+    name: 'Search',
+    icon: <Search />,
+  },
+  {
+    id: 2,
+    name: 'Explore',
+    icon: <Compass />,
+  },
+  {
+    id: 3,
+    name: 'Reels',
+    icon: <Tv />,
+  },
+  {
+    id: 4,
+    name: 'Messages',
+    icon: <Send />,
+  },
+  {
+    id: 5,
+    name: 'Notifications',
+    icon: <Heart />,
+  },
+  {
+    id: 6,
+    name: 'Create',
+    icon: <BadgePlus />,
+  },
+  {
+    id: 7,
+    name: 'Profile',
+    icon: <User />,
+  },
+];
+
+function LeftSidebar() {
+    const navItems = nav.map((item, index) => (
+        <div className="nav-item" key={index}>
+          <div className="nav-icon">{item.icon}</div>
+          <div className="nav-name">{item.name}</div>
+        </div>
+      ));
+  return (
+    <div className="left-sidebar">
+        <div className='insta-logo'>Instagram</div>
+        <div className='nav-items'>{navItems}</div>
+        
+        <div className='sub-content'></div>
+      
+    </div>
+  );
+}
+
+export default LeftSidebar;

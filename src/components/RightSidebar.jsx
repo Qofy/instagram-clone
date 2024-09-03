3d29318c36a3f39c304d3f9e3cdd00a17cac6855
Qofy <148817944+Qofy@users.noreply.github.com>
2024-09-03 07:56:04 +0200

    4th commit(right-sidebar- completed)

--- src/components/RightSidebar.jsx
+++ src/components/RightSidebar.jsx
@@ -1,11 +1,53 @@
 import '../styles/right-sidebar.css'
-import img from '../assets/profile.JPG' 
+import img from '../assets/profile.JPG'
+import antwi from '../assets/antwi.jpeg'
+import bedu from '../assets/bedu.JPG'
+import ockily from '../assets/ockily.JPG'
+import ohemaa from '../assets/ohemaa.jpg'
+import alex from '../assets/alex.JPG'
 
-const firstCompo = [{
+let firstCompo = [{
   name: 'safo_pencil_art',
   userName: 'Safo Pencil',
-  change: 'Switch'
+  change: 'Switch',
+  img: 'ohemaa',
 }]
+
+const secondCompo = [{
+  name: 'antwi-richmond',
+  mesg: 'Followed by king and 10 others',
+  change: 'Follow',
+  img: antwi,
+},
+
+{
+  name: 'alender_owu_su',
+  mesg: 'Followed by kofi and 12 others',
+  change: 'Follow',
+  img: alex,
+},
+
+{
+  name: 'iam_bedu',
+  mesg: 'Followed by yaw_beko and 10 others',
+  change: 'Follow',
+  img: bedu,
+},
+
+{
+  name: 'ockily_fy_lix',
+  mesg: 'Followed by criss_wjite and 13 others',
+  change: 'Follow',
+  img: ockily,
+},
+
+{
+  name: 'ohemaa_fio_beauty',
+  mesg: 'Followed by christo and 12 others',
+  change: 'Follow',
+  img: ohemaa,
+}
+]
 
 function RightSidebar() {
   const firstCompoItem = firstCompo.map((item, index) => {
@@ -24,12 +66,34 @@ function RightSidebar() {
       </div>
     )
   })
+
+  const secondCompoItem = secondCompo.map((item, index) => {
+    return (
+      <div className='second-compo' key={index}>
+        <div className='second-compo-img'>
+        <img src={item.img} alt="profile" />
+        </div>
+          <div className='name-titles'>
+          <h3>{item.name}</h3>
+          <p>{item.mesg}</p>
+          </div>
+        <div className='second-compo-change'>
+          <p>{item.change}</p>
+        </div>
+      </div>
+    )
+  })
   return (
     <div className='right-sidebar'>
       <div>{firstCompoItem}</div>
-    
+      <h3 className='sugest'>Suggested for you <span>See All</span></h3>
+      <div className='scnd-compo'>{secondCompoItem}</div>
+
+
+      <footer>
+      © by Safo Kofi Agyekum. Please never claim it as your own project.
+    </footer> 
     </div>
   )
 }
-
 export default RightSidebar

ee356b4626cd98f00adf66db149c7e6d70be9d16
Qofy <148817944+Qofy@users.noreply.github.com>
2024-09-07 02:10:05 +0200

    7th commit

--- src/components/MainBody.jsx
+++ src/components/MainBody.jsx
@@ -6,7 +6,7 @@ import ockily from '../assets/ockily.JPG'
 import ohemaa from '../assets/ohemaa.jpg'
 import alex from '../assets/alex.JPG'
 
-import { ChevronLeft,ChevronRight, Heart,MessageCircle,Send,Bookmark} from 'lucide-react'
+import { ChevronLeft,ChevronRight, Heart,MessageCircle,Send,Bookmark,Smile} from 'lucide-react'
 
 const storyBody = [{
   img: img ,
@@ -65,82 +65,133 @@ const contentBody = [{
   imgs: antwi,
   name: '_khelPencilArt',
   postTime: '9h',
+  sign: '...',
   post: antwi,
   like: <Heart />,
   comment: <MessageCircle />,
   share: <Send />,
   save: <Bookmark />,
-}]
+  likeNum: '1.2k likes',
+  discription: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum.',
+  commentNum: 'view all 1.2k comments',
+  addComment: 'Add a comment...',
+},
+
+{
+  imgs: bedu,
+  name: '_khelPencilArt',
+  postTime: '.9h',
+  sign: '...',
+  post: bedu,
+  like: <Heart />,
+  comment: <MessageCircle />,
+  share: <Send />,
+  save: <Bookmark />,
+  likeNum: '1.2k likes',
+  discription: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum.',
+  commentNum: 'view all 1.2k comments',
+  addComment: 'Add a comment...',
+},
+
+{
+  imgs: ockily,
+  name: '_khelPencilArt',
+  postTime: '9h',
+  sign: '...',
+  post: ockily,
+  like: <Heart />,
+  comment: <MessageCircle />,
+  share: <Send />,
+  save: <Bookmark />,
+  likeNum: '1.2k likes',
+  discription: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum.',
+  commentNum: 'view all 1.2k comments',
+  addComment: 'Add a comment...',
+}
+
+
+]
 
 function MainBody() {
-  const story = storyBody.map((story, index) => {
-    return (
-      < div className="story-content" key={index}>
-        
-          <img className='story-img' src={story.img} alt="img" />
-
-        <div className='p-name'>
-        <p className="story-name">{story.name}</p>
-        </div>
+ const storyBodyContent = storyBody.map((story, index) =>{
+  return(
+    <div className="story-body" key={index}>
+      <div className="story-body-img">
+        <img className='story-image' src={story.img} alt="" />
       </div>
-    )
-  });
+      <div className="story-body-name">
+        <p>{story.name}</p>
+      </div>
+    </div>
+  )
+ })
 
-  const content = contentBody.map((content, index) => {
-    return (
-      <div className="content-container" key={index}>
-        <div className="sub-content">
-        <div className="content-head">
-          <img src={content.imgs} alt="img" className='content-img' />
-          <div className="content-name">
+const contentBodyContent = contentBody.map((content, index) => {
+  return (
+    <div className="content-body" key={index}>
+      <div className="content-body-header">
+        <div className="name-img">
+          <img className='profile-img' src={content.imgs} alt="" />
           <p>{content.name}</p>
-          <p>&#8226;{content.postTime}</p>
+          <p>
+          &bull;{content.postTime}
+          </p>
         </div>
-        </div>
-      
-        <div className="content-post">
-          <img src={content.post} alt="post" className='item-post'/>
-        </div>
-        <div className="content-icons">
-          <div className="content-icons-left">
-            <div className="content-icons-left-left">
-              <div className="content-icons-left-left-left">
-                {content.like}
-              </div>
-              <div className="content-icons-left-left-right">
-                {content.comment}
-              </div>
-            </div>
-            <div className="content-icons-left-right">
-              {content.share}
-            </div>
+        <div className="sign">
+          {content.sign}
           </div>
-          <div className="content-icons-right">
-            {content.save}
-          </div>
-        </div>
-        </div>
       </div>
-    );
-  });
+
+      <div className="content-body-post">
+        <img className='post-img' src={content.post} alt="" />
+        </div>
+
+        <div className="post-conetent">
+          <div className="icons">
+            <div className="left-icon">
+            {content.like}
+            {content.comment}
+            {content.share}
+            </div>
+            <div>
+              {content.save}
+            </div>
+          </div>
+
+            <div className="likes">
+              {content.likeNum}
+            </div>
+            <div className="discription">
+              <p>{content.discription}</p>
+              </div>
+              <div className="comment-num">
+                <p>{content.commentNum}</p>
+                </div>
+                  <p className='add-comment'>{content.addComment}
+                  <Smile/>
+                  </p>
+
+          </div>
+
+    </div>
+  )
+})
 
   return (
-    <div className="body">
-      <div className="c1">
-        <p className="for-you">For you</p>
-        <p className='following'>Following</p>
-      </div>
-      <hr className='horizontal-row'/>
+   <div className="main-body">
+    <div className="header">
+      <p>For you</p>
+      <p className='ff'>Following</p>
+    </div>
+    <hr />
 
-      <ChevronLeft className='chevron-left'/>
-      <div className="story-container">
-        {story}
-      </div>
-      <ChevronRight className='chevron-right'/>
-
-      <div className="content">
-        {content}
-      </div>
+    <div className="story-content">
+    {storyBodyContent}
+    </div>
+<div className='body-content'>
+    {contentBodyContent}
+    </div>
+    
     </div>
   );
 }
